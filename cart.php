58c2b3d6ace008120f3503ac4144fefe3c4f63ef
Louisejesuis <louise.roy@hotmail.fr>
2018-04-15 10:44:04 +0200

    quete sessions et cookies

---
+++ cart.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <h2>Basket :</h2>
+            <?php foreach ($_COOKIE['cart'] as $basket):?>
+
+                <ul>
+                    <li><img src="assets/img/product-<?= $basket ?>.jpg"></li>
+                </ul>
+            <?php endforeach; ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
